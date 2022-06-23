c34468066de439c1b9ab15bca742e69f8775e027
Daniil Simonyan <danila718@gmail.com>
2022-06-23 15:32:39 +0300

    lesson 3

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
+app.all('/hello', (req, res, next) => {
+    console.log('All');
+    next();
+});
+
+const cb = (req, res, next) => {
+    console.log('CB');
+    next();
+}
+
+app.get('/hello', cb, (req, res) => {
     res.send('Привет!');
 });
+
 
 app.listen(port, () => {
     console.log(`Сервер запущен на http://localhost:${port}`);
