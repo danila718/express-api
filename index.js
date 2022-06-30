46095a00e2c789f5177de7d3fb2987d02c7d8f0d
Daniil Simonyan <danila718@gmail.com>
2022-06-30 11:04:14 +0300

    lesson (routing)

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.all('/hello', (req, res, next) => {
-    console.log('All');
-    next();
-});
+// app.all('/hello', (req, res, next) => {
+//     console.log('All');
+//     next();
+// });
 
-const cb = (req, res, next) => {
-    console.log('CB');
-    next();
-}
+// const cb = (req, res, next) => {
+//     console.log('CB');
+//     next();
+// }
 
-app.get('/hello', cb, (req, res) => {
-    res.send('Привет!');
+app.get('/hello', (req, res) => {
+    res.status(200).json({ success: true });
+    // res.download('/test.pdf');
 });
 
 
