4dfd624e0f9b607bb377269c708457c20cc71e91
Daniil Simonyan <danila718@gmail.com>
2022-07-01 11:32:58 +0300

    Другие типы и возможности

---
+++ ts-lessons/lesson8.ts
@@ -0,0 +1,35 @@
+let a8 = 'Привет!';
+
+if (typeof a == 'string') {
+}
+
+// зависимые типы
+let b8: typeof a8;
+
+type Coord8 = {
+    lat: number;
+    long: number;
+}
+
+// может принимать значение только ключей
+type P8 = keyof Coord8;
+
+let a81: P8 = 'long';
+
+
+function log(a: string | null): void {
+    a?.toLowerCase();
+    if (a === null) {
+    } else {
+        a.toLowerCase();
+    }
+}
+
+function log2(a: string | null): void {
+    // убеждены что будет что-то
+    a!.toLowerCase();
+}
+
+
+const a82: bigint = BigInt(100);
+const b81: symbol = Symbol('dadad');
