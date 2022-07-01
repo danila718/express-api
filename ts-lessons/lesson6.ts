b8181c73544454d0e5a32976d9acf8928d6ce5ef
Daniil Simonyan <danila718@gmail.com>
2022-07-01 09:29:05 +0300

    Generics

---
+++ ts-lessons/lesson6.ts
@@ -0,0 +1,29 @@
+// function log(obj: string | number): string | number {
+//     console.log(obj);
+//     return obj;
+// }
+
+interface HasLength {
+    length: number;
+}
+
+function log<T extends HasLength, K>(obj: T, arr: K[]): K[] {
+    // arr.length - exist
+    // obj.length - exist if T exdents HasLength
+    console.log(obj);
+    return arr;
+}
+
+log<string, number>('sadad', [12]);
+log<HasLength, number>(['s'], [5]);
+
+interface IUser {
+    name: string,
+    age?: number,
+    bid: <T>(sum: T) => boolean;
+}
+
+function bid<T>(sum: T): boolean {
+    return true;
+}
+
